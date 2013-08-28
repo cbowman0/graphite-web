c87996186142f717dc03043d893a380f99940fbf
Christopher Bowman <cbowman0@gmail.com>
2013-08-28 19:48:12 +0000

    Changes to import properly and work with Django 1.5+

--- webapp/graphite/url_shortener/models.py
+++ webapp/graphite/url_shortener/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Link(models.Model):
-    url = models.URLField(verify_exists=True, unique=True)
+    url = models.URLField(unique=True)
     date_submitted = models.DateTimeField(auto_now_add=True)
