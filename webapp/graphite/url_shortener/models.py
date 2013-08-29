b5220dd249ca6a623e30043208a0a933a7099b58
Christopher Bowman <cbowman0@gmail.com>
2013-08-29 13:50:10 +0000

    Changes to allow URL Shortener to work
    1. Convert model to use TextField so the length of the URL is not limited to 200.
    2. Remove _uniq=[0-9]* from URL
    3. Change response from /s/ to be plain text instead of HREF
    4. Add button into Dashboard UI
    5. Put window.location.origin on returned location in composer and dashboard UI.
    6. Add window.locaiton.origin on Direct URL in dashboard UI as well.

--- webapp/graphite/url_shortener/models.py
+++ webapp/graphite/url_shortener/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Link(models.Model):
-    url = models.URLField(unique=True)
+    url = models.TextField()
     date_submitted = models.DateTimeField(auto_now_add=True)
